aff95706aba29d7d0b74c1b28347f7554d4c8916
jmguerrax <58767612+jmguerrax@users.noreply.github.com>
2022-02-02 11:42:51 +0100

    Update KRR.py
    Simple code for a Kernel Ridge (Gaussian) Regression, an example of ML fit.

--- KRR/KRR.py
+++ KRR/KRR.py
@@ -1,1 +1,25 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+### Create training data: 
+
+N_tot = 200 #Total samples
+
+x_min = -1 # x-domain
+x_max = 1
+
+A_c = 1 
+x = np.linspace(x_min, x_max, N_tot) 
+
+def f_t(x):
+    #return A_c*np.cos(x)
+    return A_c*np.power(x,2)
+
+plt.plot(x,f_t(x),label='Original data')
+plt.legend(loc='upper center', shadow=True, fontsize='x-large')
+plt.xlabel('x')
+plt.ylabel('f_t(x)')
+plt.grid()
+plt.show()
 
